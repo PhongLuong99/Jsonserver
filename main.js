b872187cd88f6be6b7f9c040ec2f3e8762678469
PhongLuong99 <77917776+PhongLuong99@users.noreply.github.com>
2022-07-26 12:24:37 +0700

    hello

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ server.use(jsonServer.defaults());
 
 const SECRET_KEY = '123456789'
 
-const expiresIn = '1h'
+const expiresIn = '20s'
 // Set default middlewares (logger, static, cors and no-cache)
 //server.use(middlewares)
 
